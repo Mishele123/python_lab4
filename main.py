33fecd54a6d0ff9e6d456c06f4d75a4002e673f4
Mishele123 <misha228665161@gmail.com>
2023-11-25 15:12:14 +0400

    Создание столюцов с размерами изображений

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 
 import pandas as pd
+import cv2
 
+# tiger = 0 leopard = 1
 
 def create_DataFrame(annotation: str) -> None:
-    file = open(annotation, "r", encoding="utf-8")
+    
 
     df = pd.DataFrame(columns=["ClassName", "Directory"])
 
-    for line in file:
-        line = line.split(",")
-        df.loc[len(df.index)] = [line[2].split("\n")[0], line[0]]
-    
+    with open(annotation, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.split(",")
+            df.loc[len(df.index)] = [line[2].split("\n")[0], line[0]]
+
+    df['mark'] = 0
+
+    # Присваиваем значения 0 / 1
+    df.loc[df["ClassName"] == "tiger", "mark"] = 0
+    df.loc[df["ClassName"] == "leopard", "mark"] = 1
+
+    # Создаем столбцы с размерами изображений
+    df[["height", "width", "channels"]] = df["Directory"].apply(lambda x: pd.Series(get_image_properties(x)))
+
     print(df)
+
+def get_image_properties(img_path: str) -> int:
+    image = cv2.imread(img_path)
+    height, width, channels = image.shape
+    return height, width, channels
 
 def main() -> None:
     create_DataFrame("D:\\python_labs\\datas\\annotation.csv")
