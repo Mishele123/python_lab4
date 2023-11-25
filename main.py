edf4fd64ba81c167c04158bdc94eab6f152a1538
Mishele123 <misha228665161@gmail.com>
2023-11-25 14:29:58 +0400

    Формирование dataframe и именование колонок

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import pandas as pd
 def create_DataFrame(annotation: str) -> None:
     file = open(annotation, "r", encoding="utf-8")
 
-    for line in file:
-        print(line)
+    df = pd.DataFrame(columns=["ClassName", "Directory"])
 
+    for line in file:
+        line = line.split(",")
+        df.loc[len(df.index)] = [line[2].split("\n")[0], line[0]]
+    
+    print(df)
 
 def main() -> None:
     create_DataFrame("D:\\python_labs\\datas\\annotation.csv")
