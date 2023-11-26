d9c9a31a62099153f6510d9be48e5ebfb1dbbf23
Mishele123 <misha228665161@gmail.com>
2023-11-26 12:05:48 +0400

    res_12

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def create_DataFrame(annotation: str) -> None:
 
     print(df)
     
-    Функция вывода гистограмм
+    # Функция вывода гистограмм
     show_histogram(df, 0)
 
 def get_image_properties(img_path: str) -> int:
